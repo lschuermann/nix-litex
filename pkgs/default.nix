lschuermann/nix-litex

commit 4358e583455bef87de24c841bfd711a7db8e7ee2

pkgs: expose the maintenance scripts as a package
Expose the maintenance scripts of this repository as a separate
"maintenance" package. It is only a loose collection of Python scripts
with the proper buildInputs defined. This is useful in particular for
maintenance of downstream packages.

Signed-off-by: Leon Schuermann <leon@is.currently.online>

pkgs/default.nix
@@ -184,5 +184,34 @@ let
           "pythondata-cpu-serv"
         ]
       );
+
+  # Build a special "maintainance" package which contains tools to
+  # work with the TOML-based pkgMetas definition
+  maintenance = pkgs.python3Packages.buildPythonPackage {
+    pname = "nix-litex-maintenance";
+    version = "none";
+
+    # Simply include the entire /maintenance directory as the
+    # source. It is only a loose collection of (Python scripts), which
+    # will be copied to the $out/bin path in the installPhase.
+    src = ../maintenance;
+    format = "other";
+
+    buildInputs = [
+      pkgs.python3Packages.toml
+      pkgs.python3Packages.GitPython
+    ];
+
+    installPhase = ''
+      mkdir -p $out/bin/
+      cp *.py $out/bin/
+      chmod +x $out/bin/*
+    '';
+  };
+
 in
-pkgSet // { inherit overlay pythonOverlay; packages = pkgSet; nixpkgsExtended = extended; }
+pkgSet // {
+  inherit overlay pythonOverlay maintenance;
+  packages = pkgSet;
+  nixpkgsExtended = extended;
+}

shell.nix
@@ -42,9 +42,7 @@ pkgs.mkShell {
     icestorm
 
     # For executing the maintenance scripts of this repository
-    python3
-    python3Packages.toml
-    python3Packages.GitPython
+    maintenance
 
     # For LiteX development
     python3Packages.pytest

commit 24060aa33f38d619992806851d048d15f3a56546

pkgs/default.nix: use name instead of pname for maintenance package
This applies a recommendation by Las <me@las.rs>. Instead of
specifying the version as "none", it's better to use the `name`
attribute and don't specify a package version at all.

Fixes: 4358e583455bef ("pkgs: expose the maintenance scripts ...")
Suggested-by: Las Safin <me@las.rs>
Signed-off-by: Leon Schuermann <leon@is.currently.online>
Reviewed-by: Las Safin <me@las.rs>

pkgs/default.nix
@@ -189,8 +189,7 @@ let
   # Build a special "maintainance" package which contains tools to
   # work with the TOML-based pkgMetas definition
   maintenance = pkgs.python3Packages.buildPythonPackage {
-    pname = "nix-litex-maintenance";
-    version = "none";
+    name = "nix-litex-maintenance";
 
     # Simply include the entire /maintenance directory as the
     # source. It is only a loose collection of (Python scripts), which